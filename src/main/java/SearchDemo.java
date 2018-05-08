6c3480987b64ad7dee693da7e737752876588aea
Yumumu <46405428@qq.com>
2018-05-08 22:14:23 +0800

    代码实现

--- src/main/java/SearchDemo.java
+++ src/main/java/SearchDemo.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class SearchDemo {
+
+    private static int[] arrays = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args){
+        int number = 16;
+        System.out.println(binarySearch(arrays, 0, arrays.length - 1, number));
+    }
+
+    private static int binarySearch(int[] array, int begin, int end, int number){
+
+        if (begin > end){
+            return -1;
+        }
+
+//        int pivot = (int) Math.floor((end - begin + 1) / 2) + begin;
+        int pivot = (begin + end) >>> 1;
+
+        if (array[pivot] == number){
+            return pivot;
+        }
+
+        if (array[pivot] > number){
+
+            return binarySearch(array, begin, pivot - 1, number);
+
+        }
+
+        if (array[pivot] < number){
+
+            return binarySearch(array, pivot + 1, end, number);
+
+        }
+
+        return -1;
+    }
 }
